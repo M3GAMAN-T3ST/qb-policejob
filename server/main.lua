c68f5ed7a29579d11da0b81b85b6cb1bbca1dda5
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-02-10 19:45:00 -0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -108,13 +108,13 @@ local function DnaHash(s)
 end
 
 -- Commands
-QBCore.Commands.Add('spikestrip', Lang:t('commands.place_spike'), {}, false, function(source)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if Player.PlayerData.job.type == 'leo' and Player.PlayerData.job.onduty then
-        TriggerClientEvent('police:client:SpawnSpikeStrip', src)
-    end
-end)
+-- QBCore.Commands.Add('spikestrip', Lang:t('commands.place_spike'), {}, false, function(source)
+--     local src = source
+--     local Player = QBCore.Functions.GetPlayer(src)
+--     if Player.PlayerData.job.type == 'leo' and Player.PlayerData.job.onduty then
+--         TriggerClientEvent('police:client:SpawnSpikeStrip', src)
+--     end
+-- end)
 
 QBCore.Commands.Add('grantlicense', Lang:t('commands.license_grant'), { { name = 'id', help = Lang:t('info.player_id') }, { name = 'license', help = Lang:t('info.license_type') } }, true, function(source, args)
     local src = source
