25634dcf18d564fb859db55febc5a38fa9385519
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-02-12 18:50:09 -0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -774,7 +774,7 @@ RegisterNetEvent('police:server:JailPlayer', function(playerId, time)
     local targetPed = GetPlayerPed(playerId)
     local playerCoords = GetEntityCoords(playerPed)
     local targetCoords = GetEntityCoords(targetPed)
-    if #(playerCoords - targetCoords) > 2.5 then return DropPlayer(src, 'Attempted exploit abuse') end
+    -- if #(playerCoords - targetCoords) > 2.5 then return DropPlayer(src, 'Attempted exploit abuse') end -- ps-mdt
 
     local Player = QBCore.Functions.GetPlayer(src)
     local OtherPlayer = QBCore.Functions.GetPlayer(playerId)
